7f6719e66dbf9d9d985021e9d2556e374af583a0
Satwikagv <satwikagv@gmail.com>
2024-10-09 10:41:01 +0530

    update

--- 3/codes/circle.c
+++ 3/codes/circle.c
@@ -5,7 +5,7 @@
 #include "/home/g-v-satwika/matgeo/codes/msoft/libs/geofun.h"
 
 // Solve quadratic equation ax^2 + bx + c = 0
-void solveQuadratic(double a, double b, double c, double *root1, double *root2) {
+/*void solveQuadratic(double a, double b, double c, double *root1, double *root2) {
     double discriminant = b * b - 4 * a * c;
     if (discriminant >= 0) {
         *root1 = (-b + sqrt(discriminant)) / (2 * a);
@@ -13,7 +13,7 @@ void solveQuadratic(double a, double b, double c, double *root1, double *root2) 
     } else {
         *root1 = *root2 = 0;  
 }
-}
+}*/
 int main() {
     double **P = createMat(1, 2); // Create a point vector for P
     P[0][0] = 7;  // x-coordinate of point P
@@ -21,20 +21,24 @@ int main() {
 
     double r = 5;  // Given radius
      // Final equation: 2k^2 - 22k + 40 = 0
-    double a = 2;  // Coefficient for k^2
-    double b = -22;   // Coefficient for k
-    double c = 56;  //constant term
-    double k1, k2;
+     double **CM=createMat(3,1); //coefficient matrix
+     CM[0][0]=2;  // Coefficient for k^2
+     CM[1][0] = -22;   // Coefficient for k
+     CM[2][0] = 56;  //constant term
+
     // Solve the quadratic equation to get two values of k
-    solveQuadratic(a, b, c, &k1, &k2);
+   double **K=createMat(2,1);
+    K=Matquad(CM[0][0],CM[1][0],CM[2][0]);
+    double k1=K[0][0];
+    double k2=K[1][0];
     // Create two separate vectors for the two centers
     double **center1 = createMat(1, 2);
     double **center2 = createMat(1, 2);
     
-    center1[0][0] = k1;  // Center 1: (-k1, 1-k1)
+    center1[0][0] = k1;  // Center 1: (k1, k1-1)
     center1[0][1] = k1-1;
     
-    center2[0][0] = k2;  // Center 2: (-k2, 1-k2)
+    center2[0][0] = k2;  // Center 2: (k2, k2-1)
     center2[0][1] = k2-1;
 
     // Write the centers to a file
